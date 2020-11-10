00a839e9cb2c6fa93eae4bab7530c350d2d5121b
rafahg <r.hgar78@gmail.com>
2020-11-10 12:01:09 +0000

    Method game, created and tested

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,6 @@
 class Game
+
+  def game 
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,9 @@
 require 'game'
 
 describe Game do
-  
+  context 'methods responding to class' do
+    it '#game responds to class Game' do
+      expect(subject).to respond_to(:game)
+    end
+  end
 end
