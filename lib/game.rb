77f9a83d41ef9c6f8ed37e2a641c5434c6b68744
rafahg <r.hgar78@gmail.com>
2020-11-10 13:03:09 +0000

    Unit test for #roll_dice added, passed

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,10 @@
 class Game
 
-  def game 
+  def game  # It will control the game loop
+  end
+
+  def roll_dice # roll the dice to determine the player position.
+    1
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,5 +5,13 @@ describe Game do
     it '#game responds to class Game' do
       expect(subject).to respond_to(:game)
     end
+    it '#roll_dice responds to class Game' do
+      expect(subject).to respond_to(:roll_dice)
+    end
+  end
+  context 'method' do
+    it '#roll_dice will return an integer' do
+      expect(subject.roll_dice).to be_kind_of(Integer)
+    end
   end
 end
