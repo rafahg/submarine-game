e0ba09682014b30becac18f174027b32a5148392
rafahg <r.hgar78@gmail.com>
2020-11-10 15:34:55 +0000

    Movement method tested, unit and feature test passed

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
 class Game
 
-  attr_reader :path
+  attr_reader :path, :player_position
 
   def initialize
-    @path = (1..10).to_a
+    @path = (0..10).to_a
+    @player_position = 0
   end
 
   def game  # It will control the game loop
@@ -14,9 +15,15 @@ class Game
   end
 
   def movement
+    place = roll_dice
+    @player_position += place
+    @path[player_position] = "player"
   end
   
 
 end
 
-p a = Game.new
+a = Game.new
+a.movement
+p a.path
+p a.player_position
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,7 @@
 require 'game'
 
 describe Game do
-  before :each do
-    srand(1)
-  end
+ 
   context 'methods responding to class' do
     it '#game responds to class Game' do
       expect(subject).to respond_to(:game)
@@ -22,8 +20,15 @@ describe Game do
     end 
  
     it '#roll_dice will return a random number in the range 1..6' do
+      srand(1)
       solution = (1..6)
       expect(subject.roll_dice).to eq 6
+    end
+
+    it '#movement will return the player in the correct position' do
+      srand(2)
+      subject.movement
+      expect(subject.path).to eq [0, "player", 2, 3, 4, 5, 6, 7, 8, 9, 10]
     end
   end
 end
