eb2c6f9d92336ac814505f9b34358eac608d53cc
rafahg <r.hgar78@gmail.com>
2020-11-10 11:20:23 +0000

    Ruby projec complete setup completed

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+ruby '2.7.1'
+
+gem 'rspec', group: :test
+gem 'rubocop', '0.79.0'
+gem 'simplecov', require: false
+gem 'simplecov-console', require: false
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+require 'simplecov-console'
+require 'rspec'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter                                                                
+])
+SimpleCov.start
